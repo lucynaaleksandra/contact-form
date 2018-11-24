222856241cf40b88ef66198d0c74b96f5836381b
Lucyna Aleksandra <lucy@lucynaaleksandra.com>
2018-11-24 11:07:39 -0500

    added toggle optionn to Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,35 +4,45 @@ import PropTypes from 'prop-types'
 
 class Contact extends Component {
   state = {
-    showContactInfo: true
+    showContactInfo: false
   }
 
-  onShowClick = () => {
-    this.setState({  
-      showContactInfo: false
-    })
-  }
+  // onShowClick = () => {
+  //   this.setState({  
+  //     // toggle
+  //     showContactInfo: !this.state.showContactInfo
+  //   })
+  // }
 
   render() {
-    const { 
-      name, 
-      email, 
-      phone 
+    const {
+      name,
+      email,
+      phone
     } = this.props.contact
+
+    const { showContactInfo } = this.state
 
     return (
       <div className="card card-body mb-3">
         <h4>
           {name}{' '}
           {/* OR onClick={this.onShowClick.bind(this, name)}  */}
-          <i onClick={this.onShowClick} 
+          {/* OR onClick={this.onShowClick} */}
+          <i
+            onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
             className="fas fa-sort-down">
           </i>
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+
+        {/* toggle */}
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
+
       </div>
     );
   }
